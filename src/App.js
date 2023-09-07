59f2a4a3289498e4a9eb198af0747e684d4be885
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-09-07 09:54:56 +0300

    step-1 start project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Shop } from './components/Shop';
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Shop></Shop>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
